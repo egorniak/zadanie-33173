003033a8453c73664a6e5a2569571ca9fe6cdabd
egorniak <ewagorniak@op.pl>
2021-11-30 22:42:50 +0100

    wykonane zadanie

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let firstNumber = 5;
+let secondNumber = 10;
+
+function suma(parametr1, parametr2) {
+   let result = parametr1 + parametr2;
+    return result;
+}
+
+console.log( suma(firstNumber, secondNumber) );
